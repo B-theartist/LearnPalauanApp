7cb55dc26f22d9af7562d6726b249de9c01e4ad7
Braeden Casados <btheartist@proton.me>
2023-05-29 16:34:15 -0600

    Change content

--- App3/ContentView.swift
+++ App3/ContentView.swift
@@ -7,15 +7,107 @@
 
 import SwiftUI
 
+struct Option: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let imageName: String
+    let color: Color
+}
+
+struct QuizType: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let imageName: String
+    let color: Color
+}
+
 struct ContentView: View {
+    var options: [Option] = [
+        Option(name: "Dictionary", imageName: "book", color: .blue),
+        Option(name: "Quizzes", imageName: "square.and.pencil", color: .blue)
+    ]
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image("palauflag")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.accentColor)
+                    .cornerRadius(25)
+                    .padding(.all)
+                
+                List {
+                    Section(header: Text("Options")) {
+                        ForEach(options) { option in
+                            NavigationLink(destination: DetailView(option: option)) {
+                                Label(option.name, systemImage: option.imageName)
+                                    .foregroundColor(option.color)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Learn Palauan")
+        }
+    }
+}
+
+struct DetailView: View {
+    let option: Option
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if option.name == "Dictionary" {
+                DictionaryView()
+            } else if option.name == "Quizzes" {
+                QuizzesView()
+            }
         }
-        .padding()
+        .navigationBarTitle(option.name)
+    }
+}
+
+struct DictionaryView: View {
+    var body: some View {
+        Text("Dictionary Content")
+            .font(.largeTitle)
+    }
+}
+
+struct QuizzesView: View {
+    var options: [QuizType] = [
+        QuizType(name:"Vocab", imageName: "square.and.pencil", color: .blue),
+        QuizType(name:"Etymology", imageName: "square.and.pencil", color: .blue),
+        QuizType(name:"Reng Expressions", imageName: "square.and.pencil", color: .blue),
+        QuizType(name:"Living Things", imageName: "square.and.pencil", color: .blue),
+        QuizType(name:"Synonyms", imageName: "square.and.pencil", color: .blue),
+        QuizType(name:"Proverbs", imageName: "square.and.pencil", color: .blue),
+        QuizType(name:"Triva", imageName: "square.and.pencil", color: .blue),
+        QuizType(name:"Pronouns", imageName: "square.and.pencil", color: .blue),
+        QuizType(name:"Parts of Speech", imageName: "square.and.pencil", color: .blue)
+        
+    ]
+    
+    var body: some View {
+        List(options) { quizType in
+            NavigationLink(destination: QuizDetailView(quizType: quizType)) {
+                Label(quizType.name, systemImage: quizType.imageName)
+                    .foregroundColor(quizType.color)
+            }
+        }
+    }
+}
+
+struct QuizDetailView: View {
+    let quizType: QuizType
+    
+    var body: some View {
+        VStack {
+            Text("Quiz Type: \(quizType.name)")
+                .font(.largeTitle)
+        }
+        .navigationBarTitle(quizType.name)
     }
 }
 
@@ -24,3 +116,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+
+
+
